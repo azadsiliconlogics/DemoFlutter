07e168002ac58046c7a92b1a73d87a755ccb549a
azad <azad@siliconlogics.com>
2021-01-30 13:48:37 +0530

    user login UI

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:easy_localization/easy_localization.dart';
 
 class HomePage extends StatefulWidget {
   HomePage({Key key, this.title}) : super(key: key);
@@ -12,64 +13,43 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
+
+    var avatorImageAsset = new AssetImage("assets/images/avator.jpg");
+    var avator = new Image(
+      image: avatorImageAsset,
+      height: 200,
+      width: 160,
+    );
+
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title,
-            style: Theme.of(context).appBarTheme.textTheme.headline4),
+        title: Text("Login", style: Theme.of(context).appBarTheme.textTheme.headline4),
       ),
-      body: Center(
-          child: Container(
-        padding: EdgeInsets.only(top: 55),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            Container(
-              padding: EdgeInsets.only(top: 15, bottom: 50),
-              child: Text(
-                'Home',
-              ),
-            ),
-            FlatButton(
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(18.0),
-                  side: BorderSide(color: Colors.blue)),
-              color: Colors.white,
-              textColor: Colors.blue,
-              padding: EdgeInsets.all(8.0),
-              onPressed: () {
-                Navigator.pushNamed(context, '/users');
-              },
-              child: Text(
-                "Users",
-                style: TextStyle(
-                  fontSize: 14.0,
-                ),
-              ),
-            ),
-            FlatButton(
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(18.0),
-                  side: BorderSide(color: Colors.blue)),
-              color: Colors.white,
-              textColor: Colors.blue,
-              padding: EdgeInsets.all(8.0),
-              onPressed: () {
-                Navigator.of(context).pushNamed(
-                  '/profile',
-                  arguments:
-                      "Second Page", //This is really importan. Here you should pass every parameter as arguments
-                );
-              },
-              child: Text(
-                "Profile",
-                style: TextStyle(
-                  fontSize: 14.0,
-                ),
-              ),
-            ),
-          ],
-        ),
-      )),
+      body: Column(
+        
+        children: <Widget>[
+
+          Container(
+            padding: EdgeInsets.all(10),
+            child: avator,
+          ),
+
+          Container(
+            margin: EdgeInsets.all(10),
+            child: Text('email'.tr()),
+          ),
+
+          Container(
+            margin: EdgeInsets.all(10),
+            child: Text('password'.tr()),
+          ),
+
+          Container(
+            margin: EdgeInsets.all(10),
+          )
+
+        ],
+      ),
     );
   }
 }
